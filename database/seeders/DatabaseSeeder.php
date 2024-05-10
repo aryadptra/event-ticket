969ed08e46ceac8d8a30c5dabdcb4bfa0a50a4ea
aryadwiputra <aryadptr.business@gmail.com>
2024-05-10 21:22:43 +0700

    add user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class,
             EventsTableSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
