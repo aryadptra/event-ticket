9e4f93cb976275808b519264be86fdcf09fb2306
aryadwiputra <aryadptr.business@gmail.com>
2024-05-10 21:01:25 +0700

    create migrations

---
+++ database/migrations/2024_05_10_113720_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('headline')->nullable();
+            $table->text('description')->nullable();
+            $table->dateTime('start_time')->nullable();
+            $table->string('location')->nullable();
+            $table->integer('duration')->nullable();
+            $table->boolean('is_popular')->default(false);
+            $table->json('photos')->nullable();
+            $table->string('type')->default('offline');
+            $table->foreignId('category_id')->nullable()->constrained('categories')->onDelete('set null');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
