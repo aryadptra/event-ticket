edeecbd397ef88da9542b92fa56c194a08c13f91
aryadwiputra <aryadptr.business@gmail.com>
2024-05-10 21:04:43 +0700

    add styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import { postcss } from 'tailwindcss';
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -11,12 +12,36 @@ export default {
     ],
 
     theme: {
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: "1rem",
+                lg: "50px",
+                xl: "100px",
+            },
+        },
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                "dark-indigo": "#0D0322",
+                primary: "#27164B",
+                secondary: "#82D0DF",
+                "butter-yellow": "#F7FF82",
+                "lavender-pink": "#DF82CB",
+                "persian-pink": "#DF82CA",
+                "iron-grey": "#DAD5E4",
+                "pastel-purple": "#A99FBD",
+                "bluish-purple": "#38255F",
+                "smoke-purple": "#A497BE"
             },
         },
     },
 
-    plugins: [forms],
+    plugins: [
+        forms,
+        require('postcss-import'),
+    ],
+
 };
