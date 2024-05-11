91a89374a012731d359de3a782f84f40e8dbc554
aryadwiputra <aryadptr.business@gmail.com>
2024-05-11 12:25:49 +0700

    prepare for events feature

--- resources/js/Components/Dashboard/Sidebar.jsx
+++ resources/js/Components/Dashboard/Sidebar.jsx
@@ -1,7 +1,7 @@
 import { Link } from '@inertiajs/react';
 import React from 'react';
 import ApplicationLogo from '../ApplicationLogo';
-import { HiOutlineHome, HiOutlineTicket } from "react-icons/hi";
+import { HiOutlineHome, HiOutlineLogout, HiOutlineTicket } from "react-icons/hi";
 
 export default function Sidebar({ isOpen, toggleSidebar }) {
     return (
@@ -17,13 +17,21 @@ export default function Sidebar({ isOpen, toggleSidebar }) {
                 <div className="space-y-4">
                     <div className="px-3">
                         <div className="py-2 flex flex-col space-y-2">
-                            <Link className="flex items-center justify-start px-6 py-3 rounded-md text-lg font-semibold text-white hover:bg-gray-500 hover:text-butter-yellow" href={route('dashboard')}>
+                            <h2 className='text-white font-bold text-xl px-3 mb-2'>Menu</h2>
+                            <Link className="flex items-center justify-start px-6 py-3 rounded-md text-lg font-semibold text-white hover:bg-gray-500 hover:text-butter-yellow" href={route('admin.dashboard')}>
                                 <HiOutlineHome className="mr-2" />
                                 Dashboard
                             </Link>
-                            <Link className="flex items-center justify-start px-6 py-3 rounded-md text-lg font-semibold text-white hover:bg-gray-500 hover:text-butter-yellow" href={route('dashboard')}>
+                            <Link className="flex items-center justify-start px-6 py-3 rounded-md text-lg font-semibold text-white hover:bg-gray-500 hover:text-butter-yellow" href={route('admin.events.index')}>
                                 <HiOutlineTicket className="mr-2" />
                                 Events
+                            </Link>
+                        </div>
+                        <div className="py-6 flex flex-col space-y-2">
+                            <h2 className='text-white font-bold text-xl px-3 mb-2'>Setting</h2>
+                            <Link className="flex items-center justify-start px-6 py-3 rounded-md text-lg font-semibold text-red-500 hover:bg-gray-500 hover:text-butter-yellow" href={route('logout')} method="post" as="button">
+                                <HiOutlineLogout className="mr-2" />
+                                Logout
                             </Link>
                         </div>
                     </div>
