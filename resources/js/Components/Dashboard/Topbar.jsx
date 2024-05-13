4c8e5784a8f4c9178c9e7eaa3ebe32213256740d
aryadwiputra <aryadptr.business@gmail.com>
2024-05-13 08:44:20 +0700

    Install material ui

--- resources/js/Components/Dashboard/Topbar.jsx
+++ resources/js/Components/Dashboard/Topbar.jsx
@@ -1,8 +1,17 @@
-import { Link } from '@inertiajs/react'
+import { Avatar } from '@mui/material';
+import { Link, usePage } from '@inertiajs/react'
 import React from 'react'
 import { HiOutlineMenu } from 'react-icons/hi'
 
 export default function Topbar({ toggleSidebar }) {
+    // Data pengguna contoh
+    const { auth } = usePage().props
+
+    const user = {
+        name: auth.user.name,
+        avatarInitials: auth.user.name.split(' ').map((name) => name.charAt(0)).join(''),
+    }
+
     return (
         <div className='pb-3 mb-8 border-b flex flex-row items-center justify-between'>
             <div className='flex space-x-10'>
@@ -10,12 +19,14 @@ export default function Topbar({ toggleSidebar }) {
                     <HiOutlineMenu className="h-6 w-6" />
                 </button>
                 <div className="">
-                    <div>Company</div>
                     <div className='font-semibold'>Sinar Kasih</div>
+                    <div>Event Ticket</div>
                 </div>
             </div>
-            <div>
-                <Link href={route('admin.events.create')} className='bg-dark-indigo text-white px-6 py-3 rounded-md' >Add Event</Link>
+            <div className="flex items-center space-x-3">
+                {/* Tampilkan avatar dan nama pengguna */}
+                <Avatar>{user.avatarInitials}</Avatar>
+                <div>{user.name}</div>
             </div>
         </div>
     )
